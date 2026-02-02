6bed5178d30e66c42baff97e285b9f49be12f883
kranthi8563 <kranthireddy603@gmail.com>
2026-02-02 14:35:45 +0530

    updated files

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -2,12 +2,28 @@ package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+<<<<<<< HEAD
+=======
+import org.springframework.beans.factory.annotation.Value;
+
+>>>>>>> faeed8f (updated files)
 
 @RestController
 public class HelloController {
 
+<<<<<<< HEAD
     @GetMapping("/hello")
     public String hello() {
         return "Hello from Spring Boot + Docker + Jenkins!";
     }
+=======
+   @Value("${app.message}")
+   private String message;
+
+   @GetMapping("/hello")
+   public String hello() {
+      return message;
+    }
+
+>>>>>>> faeed8f (updated files)
 }
